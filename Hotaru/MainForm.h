09535065c3a37c0cd6fc2ab6af39b9a3fa3d60bf
Andrew Edwards <aje5218@rit.edu>
2014-12-12 10:57:58 -0500

    Came to a roadblock in original project. Starting over in C# with HotaruV2

--- Hotaru/MainForm.h
+++ Hotaru/MainForm.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <tsk/libtsk.h>
+#include <cstring>
+#include <string>
+#include <vcclr.h>
 
 namespace Hotaru {
 
@@ -16,6 +19,7 @@ namespace Hotaru {
 	/// </summary>
 	public ref class MainForm : public System::Windows::Forms::Form
 	{
+		static System::Collections::Specialized::StringCollection ^ log = gcnew System::Collections::Specialized::StringCollection();
 		public:
 			MainForm(void)
 			{
@@ -51,6 +55,7 @@ namespace Hotaru {
 		private: System::Windows::Forms::TreeView^  treeView;
 
 		private: System::Windows::Forms::ImageList^  imageList;
+		private: System::Windows::Forms::SplitContainer^  splitContainer1;
 		private: System::ComponentModel::IContainer^  components;
 
 
@@ -76,12 +81,16 @@ namespace Hotaru {
 				this->exitMenu = (gcnew System::Windows::Forms::ToolStripMenuItem());
 				this->openFileDialog = (gcnew System::Windows::Forms::OpenFileDialog());
 				this->splitContainer = (gcnew System::Windows::Forms::SplitContainer());
+				this->splitContainer1 = (gcnew System::Windows::Forms::SplitContainer());
 				this->treeView = (gcnew System::Windows::Forms::TreeView());
 				this->imageList = (gcnew System::Windows::Forms::ImageList(this->components));
 				this->menuStrip->SuspendLayout();
 				(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer))->BeginInit();
 				this->splitContainer->Panel1->SuspendLayout();
 				this->splitContainer->SuspendLayout();
+				(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->BeginInit();
+				this->splitContainer1->Panel1->SuspendLayout();
+				this->splitContainer1->SuspendLayout();
 				this->SuspendLayout();
 				// 
 				// menuStrip
@@ -91,7 +100,7 @@ namespace Hotaru {
 				this->menuStrip->Location = System::Drawing::Point(0, 0);
 				this->menuStrip->Name = L"menuStrip";
 				this->menuStrip->RenderMode = System::Windows::Forms::ToolStripRenderMode::System;
-				this->menuStrip->Size = System::Drawing::Size(849, 24);
+				this->menuStrip->Size = System::Drawing::Size(903, 24);
 				this->menuStrip->TabIndex = 0;
 				this->menuStrip->Text = L"menuStrip1";
 				// 
@@ -128,13 +137,27 @@ namespace Hotaru {
 				this->splitContainer->Dock = System::Windows::Forms::DockStyle::Fill;
 				this->splitContainer->Location = System::Drawing::Point(0, 24);
 				this->splitContainer->Name = L"splitContainer";
+				this->splitContainer->Orientation = System::Windows::Forms::Orientation::Horizontal;
 				// 
 				// splitContainer.Panel1
 				// 
-				this->splitContainer->Panel1->Controls->Add(this->treeView);
-				this->splitContainer->Size = System::Drawing::Size(849, 468);
-				this->splitContainer->SplitterDistance = 283;
+				this->splitContainer->Panel1->Controls->Add(this->splitContainer1);
+				this->splitContainer->Size = System::Drawing::Size(903, 529);
+				this->splitContainer->SplitterDistance = 367;
 				this->splitContainer->TabIndex = 1;
+				// 
+				// splitContainer1
+				// 
+				this->splitContainer1->Dock = System::Windows::Forms::DockStyle::Fill;
+				this->splitContainer1->Location = System::Drawing::Point(0, 0);
+				this->splitContainer1->Name = L"splitContainer1";
+				// 
+				// splitContainer1.Panel1
+				// 
+				this->splitContainer1->Panel1->Controls->Add(this->treeView);
+				this->splitContainer1->Size = System::Drawing::Size(903, 367);
+				this->splitContainer1->SplitterDistance = 301;
+				this->splitContainer1->TabIndex = 1;
 				// 
 				// treeView
 				// 
@@ -144,7 +167,7 @@ namespace Hotaru {
 				this->treeView->Location = System::Drawing::Point(0, 0);
 				this->treeView->Name = L"treeView";
 				this->treeView->SelectedImageIndex = 0;
-				this->treeView->Size = System::Drawing::Size(283, 468);
+				this->treeView->Size = System::Drawing::Size(301, 367);
 				this->treeView->TabIndex = 0;
 				// 
 				// imageList
@@ -159,7 +182,7 @@ namespace Hotaru {
 				this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 				this->AutoScroll = true;
 				this->BackColor = System::Drawing::SystemColors::Control;
-				this->ClientSize = System::Drawing::Size(849, 492);
+				this->ClientSize = System::Drawing::Size(903, 553);
 				this->Controls->Add(this->splitContainer);
 				this->Controls->Add(this->menuStrip);
 				this->MainMenuStrip = this->menuStrip;
@@ -171,6 +194,9 @@ namespace Hotaru {
 				this->splitContainer->Panel1->ResumeLayout(false);
 				(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer))->EndInit();
 				this->splitContainer->ResumeLayout(false);
+				this->splitContainer1->Panel1->ResumeLayout(false);
+				(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->EndInit();
+				this->splitContainer1->ResumeLayout(false);
 				this->ResumeLayout(false);
 				this->PerformLayout();
 
@@ -179,36 +205,48 @@ namespace Hotaru {
 		#pragma endregion
 		private: System::Void addImageMenu_Click(System::Object^  sender, System::EventArgs^  e) {
 				OpenFileDialog ^ openImage = gcnew OpenFileDialog();
-				openImage->Filter = "Image Files|*.dd, *.img, *.raw|All Files|*.*";
-				openImage->ShowDialog();	
+				openImage->Filter = "Image Files|*.dd, *.img, *.raw, *.aff|All Files|*.*";
+				if (openImage->ShowDialog() == System::Windows::Forms::DialogResult::OK){
+					//TskImgInfo * image = tsk_img_open_sing(
+				}
 		}
 
 		private: System::Void exitMenu_Click(System::Object^  sender, System::EventArgs^  e) {
 				Application::Exit();
 		}
+		
+		private: System::Void PopulateFolders(String ^ dir, TreeNode ^ node){
+				DirectoryInfo ^ directory = gcnew DirectoryInfo(dir);
+				try{
+					array<DirectoryInfo^>^ directories = directory->GetDirectories();
+					for each (DirectoryInfo ^ d in directories)
+					{
+						TreeNode ^ t = gcnew TreeNode(d->Name);
+						PopulateFolders(d->FullName, t);
+						node->Nodes->Add(d->Name);
+					}
+				}
+				catch (System::Exception ^ e){
+					return;
+				}
+		}
+
 		private: System::Void MainForm_Load(System::Object^  sender, System::EventArgs^  e) {
+				// Lists the drives and folders on the system
 				for each (DriveInfo^ drive in DriveInfo::GetDrives())
 				{
 					TreeNode ^ newNode = gcnew TreeNode(drive->Name);
 					treeView->Nodes->Add(newNode);
-					treeView->SelectedNode = newNode;
-					DirectoryInfo ^ directoryInfo = gcnew DirectoryInfo(drive->Name);
-
-					if (Directory::Exists(drive->Name)){
-						try{
-							array<DirectoryInfo^>^ directories = directoryInfo->GetDirectories();
-							if (directories->Length > 0){
-								for each (DirectoryInfo^ d in directories)
-								{
-									TreeNode ^ node = treeView->SelectedNode->Nodes->Add(d->Name);
-								}
-							}
+					/*DirectoryInfo ^ directoryInfo = gcnew DirectoryInfo(drive->Name);
+					array<DirectoryInfo^>^ directories = directoryInfo->GetDirectories();
+					if (directories->Length > 0){
+						for each (DirectoryInfo^ d in directories)
+						{
+							newNode->Nodes->Add(d->Name);
 						}
-						catch (Exception ^ ex){
-							MessageBox::Show(ex->Message);
-						}
-					}
+					}*/
+					PopulateFolders(drive->RootDirectory->FullName, newNode);
 				}
 		}
-};
+	};
 }
