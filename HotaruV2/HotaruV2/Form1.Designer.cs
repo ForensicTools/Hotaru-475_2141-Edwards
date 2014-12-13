2777d709320ef89a86a7cc5834c3fd323483101b
Andrew Edwards <aje5218@rit.edu>
2014-12-12 21:17:35 -0500

    File browser and list working

--- HotaruV2/HotaruV2/Form1.Designer.cs
+++ HotaruV2/HotaruV2/Form1.Designer.cs
@@ -29,11 +29,169 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listView = new System.Windows.Forms.ListView();
+            this.fileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.fileType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.fullPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView);
+            this.splitContainer1.Size = new System.Drawing.Size(759, 551);
+            this.splitContainer1.SplitterDistance = 373;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.treeView);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.chart);
+            this.splitContainer2.Size = new System.Drawing.Size(759, 373);
+            this.splitContainer2.SplitterDistance = 216;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // treeView
+            // 
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView.ImageIndex = 0;
+            this.treeView.ImageList = this.imageList;
+            this.treeView.Location = new System.Drawing.Point(0, 0);
+            this.treeView.Name = "treeView";
+            this.treeView.SelectedImageIndex = 0;
+            this.treeView.Size = new System.Drawing.Size(212, 369);
+            this.treeView.TabIndex = 0;
+            this.treeView.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterExpand);
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "drive.png");
+            this.imageList.Images.SetKeyName(1, "Folder-icon.png");
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(0, 0);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(535, 369);
+            this.chart.TabIndex = 0;
+            this.chart.Text = "chart";
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileName,
+            this.fileType,
+            this.fullPath});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.Location = new System.Drawing.Point(0, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(755, 170);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView_ColumnClick);
+            this.listView.DoubleClick += new System.EventHandler(this.listView_DoubleClick);
+            // 
+            // fileName
+            // 
+            this.fileName.Text = "File Name";
+            this.fileName.Width = 111;
+            // 
+            // fileType
+            // 
+            this.fileType.Text = "Type/Extension";
+            this.fileType.Width = 100;
+            // 
+            // fullPath
+            // 
+            this.fullPath.Text = "Path to File";
+            this.fullPath.Width = 307;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(759, 551);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Hotaru";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader fileName;
+        private System.Windows.Forms.ColumnHeader fileType;
+        private System.Windows.Forms.ColumnHeader fullPath;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
     }
 }
 
--- HotaruV2/HotaruV2/Form1.cs
+++ HotaruV2/HotaruV2/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,111 @@ namespace HotaruV2
 {
     public partial class Form1 : Form
     {
+        private ListViewColumnSorter lvwColumnSorter;
+
         public Form1()
         {
             InitializeComponent();
+            PopulateTreeView();
+            // Create an instance of a ListView column sorter and assign it 
+            // to the ListView control.
+            lvwColumnSorter = new ListViewColumnSorter();
+            this.listView.ListViewItemSorter = lvwColumnSorter;
+        }
+
+        private void PopulateTreeView()
+        {
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                TreeNode rootNode = new TreeNode(drive.Name);
+                rootNode.ImageIndex = rootNode.SelectedImageIndex = 0;
+                rootNode.Tag = drive.Name;
+                treeView.Nodes.Add(rootNode);
+                rootNode.Nodes.Add("");
+            }
+        }
+
+        private static void ShowAllFoldersUnder(string path, TreeNode parent, int i)
+        {
+            int count = i;
+            if (count < 3)
+            {
+                count++;
+                try
+                {
+                    if (((File.GetAttributes(path) & FileAttributes.ReparsePoint)
+                        != FileAttributes.ReparsePoint) /*&& ((File.GetAttributes(path) & FileAttributes.System) != FileAttributes.System)*/)
+                    {
+                        foreach (string folder in Directory.GetDirectories(path))
+                        {
+                            TreeNode child = new TreeNode(folder);
+                            child.ImageIndex = child.SelectedImageIndex = 1;
+                            child.Tag = folder;
+                            ShowAllFoldersUnder(folder, child, count);
+                            parent.Nodes.Add(child);
+                        }
+                    }
+                }
+                catch (UnauthorizedAccessException) { }
+            }
+        }
+
+        private void treeView_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            ShowAllFoldersUnder(e.Node.Tag.ToString(), e.Node, 0);
+        }
+
+        //Clears the current list of files and displays all file information in the selected folder
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listView.Items.Clear();
+            try
+            {
+                foreach (String file in Directory.GetFiles(e.Node.Tag.ToString()))
+                {
+                    ListViewItem lvi = new ListViewItem(Path.GetFileNameWithoutExtension(file));
+                    lvi.SubItems.Add(Path.GetExtension(file));
+                    lvi.SubItems.Add(file);
+                    listView.Items.Add(lvi);
+                }
+            }
+            catch (UnauthorizedAccessException) { }
+        }
+
+        private void listView_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            // Determine if clicked column is already the column that is being sorted.
+            if (e.Column == lvwColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (lvwColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvwColumnSorter.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    lvwColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                lvwColumnSorter.SortColumn = e.Column;
+                lvwColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            this.listView.Sort();
+        }
+
+        private void listView_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count == 1)
+            {
+
+                Process.Start(listView.SelectedItems[0].SubItems[2].Text);                
+            }
         }
     }
 }
